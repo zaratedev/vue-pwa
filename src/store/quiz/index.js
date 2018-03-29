e1ade94d722919625be9be87eca081f4ddbe59d5
Jonathan Zarate <zaratedev@gmail.com>
2018-03-29 13:21:06 -0600

    Initial commit

---
+++ src/store/quiz/index.js
@@ -0,0 +1,22 @@
+const state = {
+	quiz: 'a funny quiz'
+};
+const getters = {
+
+};
+
+const mutations = {
+
+};
+
+const actions = {
+
+};
+
+export default {
+	namespaced: true,
+	state,
+	getters,
+	mutations,
+	actions
+}
